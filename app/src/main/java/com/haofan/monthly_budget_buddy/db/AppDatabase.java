8bb442e436415ecb072789d42314d090c42b16b8
Haofan <haofan.hou@gmail.com>
2018-04-05 16:17:49 -0300

    db built

--- app/src/main/java/com/haofan/monthly_budget_buddy/db/AppDatabase.java
+++ app/src/main/java/com/haofan/monthly_budget_buddy/db/AppDatabase.java
@@ -1,4 +1,30 @@
 package com.haofan.monthly_budget_buddy.db;
 
-public class AppDatabase {
+import android.arch.persistence.db.SupportSQLiteOpenHelper;
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.DatabaseConfiguration;
+import android.arch.persistence.room.InvalidationTracker;
+import android.arch.persistence.room.RoomDatabase;
+
+import com.haofan.monthly_budget_buddy.Daos.ItemDao;
+import com.haofan.monthly_budget_buddy.Daos.MonthlyStatusDao;
+import com.haofan.monthly_budget_buddy.entities.Item;
+import com.haofan.monthly_budget_buddy.entities.MonthlyStatus;
+
+@Database(entities = {Item.class, MonthlyStatus.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase{
+
+    public abstract ItemDao itemDao();
+
+    public abstract MonthlyStatusDao monthlyStatusDao();
+    
+    @Override
+    protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration config) {
+        return null;
+    }
+
+    @Override
+    protected InvalidationTracker createInvalidationTracker() {
+        return null;
+    }
 }
